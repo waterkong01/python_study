f9f5b979137988fe632d82efb9e4bcf29c98f4f0
waterkong01 <hsubin615@naver.com>
2024-09-13 16:39:38 +0900

    240913 update

--- python/KH_basic/딕셔너리커피메뉴.py
+++ python/KH_basic/딕셔너리커피메뉴.py
@@ -1,6 +1,11 @@
 # 커피 메뉴 만들기
-# [1] 메뉴 보기 [2] 메뉴 조회 [3] 메뉴 추가 [4] 메뉴 삭제 [5] 종료하기
+# [1] 메뉴 보기 [2] 메뉴 조회 [3] 메뉴 추가 [4] 메뉴 삭제 [5] 파일 불러오기 [6] 저장하기 [7] 종료 하기
 # 기본 메뉴 만들기
+import json
+
+# from anaconda_navigator.utils.url_utils import file_name
+from panel.io.resources import json_dumps
+
 menu = {
     "Americano": ["Coffee", 2000, "기본 커피 입니다."],
     "Latte": ["Coffee", 4000, "라떼 입니다."],
@@ -9,6 +14,22 @@ menu = {
     "BlackTea": ["Tea", 4500, "홍차 입니다."],
     "GreenTea": ["Tea", 4500, "녹차 입니다."]
 }
+file_name = "menu.json"
+
+# 파일에서 메뉴를 읽어오는 함수
+def load_menu():
+    try:
+        with open(file_name, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("해당 파일이 없습니다.")
+    except json.JSONDecodeError:
+        print("JSON 디코딩 실패")
+        
+# 파일에 저장하는 함수
+def save_menu():
+    with open(file_name, "w", encoding="utf-8") as file:
+        json.dump(menu, file, ensure_ascii=False, indent=4)
 
 # [1] 메뉴 보기
 def print_menu():
@@ -39,3 +60,30 @@ def del_menu(key):
         print(f"{key} 메뉴가 삭제 되었습니다.")
     else:
         print("삭제할 메뉴가 없습니다.")
+
+while True:
+    print("메뉴를 선택 하세요 : ")
+    sel = input("[1]메뉴 보기 [2] 메뉴 조회 [3] 메뉴 추가 [4] 메뉴 삭제 [5] 파일 불러오기 [6] 저장하기 [7] 종료하기 : ")
+    if sel == "1":
+        print_menu()
+    elif sel == "2":
+        key = input("조회할 메뉴 입력 : ")
+        get_menu(key)
+    elif sel == "3":
+        key = input("추가할 메뉴 이름 : ")
+        cate = input("분류 입력 : ")
+        price = int(input("가격 입력 : "))
+        desc = input("메뉴 설명 입력 : ")
+        add_menu(key, cate, price, desc)
+    elif sel == "4":
+        key = input("삭제할 메뉴 입력 : ")
+        del_menu(key)
+    elif sel == "5":
+        load_menu()
+    elif sel == "6":
+        save_menu()
+    elif sel == "7":
+        print("영업을 종료 합니다.")
+        break
+    else:
+        print("잘못된 입력입니다.")
